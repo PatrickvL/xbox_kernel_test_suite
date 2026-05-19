a0c17175f7fbbc8d7bfa76d1b651cb89a83fe49b
PatrickvL <PatrickvL@users.noreply.github.com>
2026-05-19 13:48:37 +0200

    Add shared test definitions and constants
    Add REG_DWORD, IRQL levels, SLIST_ENTRY, KOBJECTS enum, status codes,
    IRP major function codes, and other defines needed across test files.

--- src/assertions/defines.h
+++ src/assertions/defines.h
@@ -1,6 +1,65 @@
 #pragma once
 
+#include <excpt.h>
 #include "util/output.h"
+
+// Registry type constants not provided by nxdk
+#ifndef REG_DWORD
+#define REG_DWORD 4
+#endif
+
+#ifndef STATUS_CANCELLED
+#define STATUS_CANCELLED ((NTSTATUS)0xC0000120L)
+#endif
+
+#ifndef DUPLICATE_CLOSE_SOURCE
+#define DUPLICATE_CLOSE_SOURCE 0x00000001
+#endif
+
+#ifndef IRP_MJ_READ
+#define IRP_MJ_READ 0x03
+#endif
+
+#ifndef IRP_MJ_WRITE
+#define IRP_MJ_WRITE 0x04
+#endif
+
+#ifndef STATUS_SHARING_VIOLATION
+#define STATUS_SHARING_VIOLATION ((NTSTATUS)0xC0000043L)
+#endif
+
+#ifndef STATUS_INVALID_DEVICE_REQUEST
+#define STATUS_INVALID_DEVICE_REQUEST ((NTSTATUS)0xC0000010L)
+#endif
+
+// IRQL levels not provided by nxdk
+#ifndef PROFILE_LEVEL
+#define PROFILE_LEVEL 27
+#endif
+#ifndef HIGH_LEVEL
+#define HIGH_LEVEL 31
+#endif
+
+// SLIST_ENTRY alias not provided by nxdk
+#ifndef _SLIST_ENTRY_DEFINED
+#define _SLIST_ENTRY_DEFINED
+typedef SINGLE_LIST_ENTRY SLIST_ENTRY, *PSLIST_ENTRY;
+#endif
+
+// Kernel dispatcher object types not provided by nxdk
+#ifndef _KOBJECTS_DEFINED
+#define _KOBJECTS_DEFINED
+typedef enum _KOBJECTS {
+    EventNotificationObject = 0,
+    EventSynchronizationObject = 1,
+    MutantObject = 2,
+    QueueObject = 4,
+    SemaphoreObject = 5,
+    ThreadObject = 6,
+    TimerNotificationObject = 8,
+    TimerSynchronizationObject = 9,
+} KOBJECTS;
+#endif
 
 #define ASSERT_HEADER BOOL test_passed = 1;
 
