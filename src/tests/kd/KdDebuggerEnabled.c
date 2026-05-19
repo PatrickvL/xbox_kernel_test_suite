b9ea93804c3611bea10e73d2713659dba06c3d7e
PatrickvL <PatrickvL@users.noreply.github.com>
2026-05-19 13:49:37 +0200

    Implement Kd tests: debugger state exports
    - KdDebuggerEnabled: verify boolean state is 0 or 1
    - KdDebuggerNotPresent: verify boolean state is 0 or 1

--- src/tests/kd/KdDebuggerEnabled.c
+++ src/tests/kd/KdDebuggerEnabled.c
@@ -1,8 +1,16 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 #include "util/output.h"
+#include "assertions/defines.h"
 
 TEST_FUNC(KdDebuggerEnabled)
 {
-    /* FIXME: This is a stub! implement this function! */
+    TEST_BEGIN();
+
+    // KdDebuggerEnabled is a BOOLEAN exported variable
+    // It should be either TRUE or FALSE (valid boolean)
+    BOOLEAN enabled = KdDebuggerEnabled;
+    GEN_CHECK(enabled == TRUE || enabled == FALSE, TRUE, "valid boolean value");
+
+    TEST_END();
 }
--- src/tests/kd/KdDebuggerNotPresent.c
+++ src/tests/kd/KdDebuggerNotPresent.c
@@ -1,8 +1,23 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 #include "util/output.h"
+#include "assertions/defines.h"
 
 TEST_FUNC(KdDebuggerNotPresent)
 {
-    /* FIXME: This is a stub! implement this function! */
+    TEST_BEGIN();
+
+    // KdDebuggerNotPresent is a BOOLEAN exported variable
+    // It should be either TRUE or FALSE (valid boolean)
+    BOOLEAN not_present = KdDebuggerNotPresent;
+    GEN_CHECK(not_present == TRUE || not_present == FALSE, TRUE, "valid boolean value");
+
+    // KdDebuggerNotPresent should be the logical opposite of KdDebuggerEnabled
+    // (when debugger is enabled, it is present; when not present, it is not enabled)
+    BOOLEAN enabled = KdDebuggerEnabled;
+    if (enabled) {
+        GEN_CHECK(not_present, FALSE, "debugger enabled means present");
+    }
+
+    TEST_END();
 }
