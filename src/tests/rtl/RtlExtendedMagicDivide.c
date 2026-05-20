8b0876efea7ac3e3537afe692abdd31b26a935b6
PatrickvL <PatrickvL@users.noreply.github.com>
2026-05-20 12:14:19 +0200

    Fix RtlExtendedMagicDivide test: use correct magic divisor constant

--- src/tests/rtl/RtlExtendedMagicDivide.c
+++ src/tests/rtl/RtlExtendedMagicDivide.c
@@ -18,14 +18,14 @@ TEST_FUNC(RtlExtendedMagicDivide)
 
     // RtlExtendedMagicDivide computes: (Dividend * MagicDivisor) >> (64 + ShiftCount)
     // This is used internally for time conversions.
-    // Magic number for dividing by 10000000 (100ns to seconds): 0x0000000089705F41, shift=23
+    // Magic number for dividing by 10000000 (100ns to seconds), from NT kernel time.c
     magic_div_test tests[] = {
         // 10000000 / 10000000 = 1 (using magic for div by 10000000)
-        { .dividend.QuadPart = 10000000LL, .magic_divisor.QuadPart = 0x89705F4136B4A597LL, .shift_count = 23, .expected_result = 1 },
+        { .dividend.QuadPart = 10000000LL, .magic_divisor.QuadPart = (LONGLONG)0xd6bf94d5e57a42bdULL, .shift_count = 23, .expected_result = 1 },
         // 100000000 / 10000000 = 10
-        { .dividend.QuadPart = 100000000LL, .magic_divisor.QuadPart = 0x89705F4136B4A597LL, .shift_count = 23, .expected_result = 10 },
+        { .dividend.QuadPart = 100000000LL, .magic_divisor.QuadPart = (LONGLONG)0xd6bf94d5e57a42bdULL, .shift_count = 23, .expected_result = 10 },
         // 0 / anything = 0
-        { .dividend.QuadPart = 0, .magic_divisor.QuadPart = 0x89705F4136B4A597LL, .shift_count = 23, .expected_result = 0 },
+        { .dividend.QuadPart = 0, .magic_divisor.QuadPart = (LONGLONG)0xd6bf94d5e57a42bdULL, .shift_count = 23, .expected_result = 0 },
         // Simple test: magic for dividing by 10: magic=0xCCCCCCCCCCCCCCCD, shift=3
         { .dividend.QuadPart = 100, .magic_divisor.QuadPart = (LONGLONG)0xCCCCCCCCCCCCCCCDULL, .shift_count = 3, .expected_result = 10 },
         { .dividend.QuadPart = 1000, .magic_divisor.QuadPart = (LONGLONG)0xCCCCCCCCCCCCCCCDULL, .shift_count = 3, .expected_result = 100 },
